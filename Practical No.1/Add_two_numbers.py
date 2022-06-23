9a438d529f2d5b66e08855ce0e76050c6d94c557
aartipardeshi <103304372+aartipardeshi@users.noreply.github.com>
2022-06-23 21:48:49 +0530

    Add files via upload

---
+++ Practical No.1/Add_two_numbers.py
@@ -0,0 +1,7 @@
+#Program to add two numbers
+
+a=14
+b=5
+c=a+b
+print("Addition of {0} and {1} is {2}: " .format(a,b,c))
+
---
+++ Practical No.1/km_to_milles.py
@@ -0,0 +1,9 @@
+
+#Program to convert kilometers to miles
+
+kilometers=64
+conv_factor=0.62137
+
+miles= kilometers * conv_factor
+print("The conversion of kilometers to miles is :" , miles)
+
