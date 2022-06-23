30c787bb29004561a261f5a88e0bab988269095f
aartipardeshi <103304372+aartipardeshi@users.noreply.github.com>
2022-06-23 22:16:51 +0530

    Delete consecutive_common_number.py

--- consecutive_common_number.py
+++
@@ -1,9 +0,0 @@
-# To check if the list contain 3 consecutive common numbers
-
-list= [ 3, 3, 3, 5, 12, 13, 7]
-print("Input list is :", list)
-size = len(list)
-
-for i in range(size - 2):
-        if list[i] == list[i + 1] and list[i + 1] == list[i + 2]:
-                print("The 3 consecutive common number in list : ",list[i])
